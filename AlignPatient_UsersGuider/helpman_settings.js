ce4ff4c6137ebb3009f0a5c90dff23f124b150d7
Valeriy Onuchin <va.onuchin@mail.ru>
2026-03-10 16:32:52 +0300

    add alighn patient help

---
+++ AlignPatient_UsersGuider/helpman_settings.js
@@ -0,0 +1,7 @@
+/* Project settings */
+var hmForceRedirect = true;
+var hmAnimate = true;
+var hmPopupSticky = true;
+var hmImageLightbox = true;
+var hmVideoLightbox = true;
+var hmLightboxConstrained = true;
